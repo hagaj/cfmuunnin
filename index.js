b7adbf654940d3e4d021e102cf5b4cd8ff79b5eb
Joakim Haga <joakim.haga@seamk.fi>
2021-09-17 00:13:48 +0300

    syöte korjattu

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-function muunna() {
-    alert('täällä ollaan');
-}
-
 const fahrenheitSyote = document.querySelector('input');
 const fahrenheitTuloste = document.querySelector('#fahrenheit');
 const celsiusTuloste = document.querySelector('#celsius');
 const muunnaButton = document.querySelector('button');
 
+
+function fahrenheitCelsiukseksi(fahrenheit) {
+    let celsius = fahrenheit;
+    return celsius;
+}
+
 function muunna() {
     let syote = fahrenheitSyote.value;
     fahrenheitTuloste.textContent = syote;
+
+    celsiusTuloste.textContent = fahrenheitCelsiukseksi(syote);
 }
 
 muunnaButton.addEventListener('click', muunna);
